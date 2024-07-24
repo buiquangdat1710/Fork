161fa234d132c817a5c98a8cca196e78e98b96ba
HailamAICT <hailam04.work@gmail.com>
2024-07-24 23:09:00 +0700

    Update Shell

--- run.sh
+++ run.sh
@@ -6,7 +6,5 @@ conda activate agent
 
 # Start pm2 process and save logs
 echo "Training the model"
-pm2 start train.py --name buiquangdat --log-date-format "YYYY-MM-DD HH:mm Z" --output logs/train-out.log --error logs/train-error.log
 
-# Optionally, you can use pm2 to manage your logs
-pm2 logs train-model
+python train.py
