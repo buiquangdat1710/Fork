000df91034eda1cfdb40abf4eca9554065cc5291
HailamAICT <hailam04.work@gmail.com>
2024-07-29 20:55:50 +0700

    Update log file

--- Scripts/crontab.sh
+++ Scripts/crontab.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /home/naver/Individual/Extened/CodeSage
-git add /home/naver/Individual/Extened/CodeSage/log.log
+git add /home/naver/Individual/Extened/CodeSage/output.log
 git commit -m "Update log file"
 git push origin main
 
